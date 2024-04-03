db3b6bcd2f3210a9e8f2713c85208ed8b94e1988
Tech-goodat <felixkiprotich2000@gmail.com>
2024-04-03 17:35:35 +0300

    building flask applications code along

--- server/app.py
+++ server/app.py
@@ -1,5 +1,14 @@
-#!/usr/bin/env python3
-
 from flask import Flask
 
-app = Flask(__name__)
+app=Flask(__name__)
+
+@app.route('/')
+def index():
+    return '<h1>This is your homepage</h1>'
+
+@app.route('/<string:username>')
+def user(username):
+    return f'<h1>your name is {username}</h1>'
+
+if __name__=='__main__':
+    app.run(port=5555)
